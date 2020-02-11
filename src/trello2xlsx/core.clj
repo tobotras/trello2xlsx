94fa37c749c9ead5b3d09cafb4434d96ae99138f
Boris Tobotras <boris@xtalk.msk.su>
2020-02-11 12:48:36 +0300

    Ignore closed cards

--- src/trello2xlsx/core.clj
+++ src/trello2xlsx/core.clj
@@ -3,22 +3,30 @@
             [dk.ative.docjure.spreadsheet :as sheet])  
   (:gen-class))
 
-(defn- bad-json []
+(defn- log [s]
   (binding [*out* *err*]
-    (println "Cannot parse JSON"))
+    (println "LOG:" s)))
+
+(defn- bad-json []
+  (log "Cannot parse JSON")
   (System/exit 2))
 
-(defn- process-card [data card]
+(defn- process-card [card]
   (if-let [name (get card "name")]
     [name (get card "shortUrl") (get card "desc")]
     (bad-json)))
 
+(defn- eligible-card? [listId card]
+  (and (= (get card "idList") listId)
+       (not (get card "closed"))))
+
 (defn- process-list [data l]
   (if-let [name (get l "name")]
     [name 
-     (let [id (get l "id")]
-       (vec (map #(process-card data %)
-                 (filter #(= (get % "idList") id) (get data "cards")))))]
+     (let [id (get l "id")
+           eligible-cards (filter #(eligible-card? id %)
+                                  (get data "cards"))]
+       (mapv process-card eligible-cards))]
     (bad-json)))
   
 (defn- process-json [data out-file]
@@ -26,14 +34,18 @@
     (sheet/save-workbook!
      out-file
      (apply sheet/create-workbook
-            (apply concat (map #(process-list data %) lists))))
+            (apply concat
+                   (map #(process-list data %)
+                        lists))))
     (bad-json)))
 
 (defn -main [& args]
   (println "trello2xlsx, (c) Boris Tobotras, boris@xtalk.msk.su, 2020")
+  
   (when-not (= (count args) 2)
     (println "Usage: trello2xlsx source.json destination.xlsx")
     (System/exit 1))
+  
   (with-open [input (clojure.java.io/reader (first args))]
     (if-let [data (json/read input)]
       (process-json data (second args))
